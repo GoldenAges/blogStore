3cf2dca88c626574a6d67e919d989bcedfb72809
Richard <fengjiangyao@foxmail.com>
2017-02-23 17:40:55 +0800

    update

--- git/git基础使用.md
+++ git/git基础使用.md
@@ -40,9 +40,9 @@
 #一次完整的工作流
 * git clone project
 * 本地修改，添加到跟踪区 git -add file（撤销操作见后文）
-* 提交到缓存区 git -commit -m "message" （撤销操作票见后文）
+* 提交到缓存区 git -commit -m "message" （撤销操见后文）
 * 将服务器的拉取到本地 git pull origin master(当前分支)
-* 提交到服务器 git push origin master(当前分支)
+* 提交到服务器 git push -u origin master(当前分支)
 * 提交报错 conflict, 本地解决冲突后再次提交
 
 #Git的一些疑问以及解答
---
+++ 时间戳转化.php
@@ -0,0 +1,39 @@
+<?php 
+date_default_timezone_set('Asia/Shanghai');
+var_dump(strlen('123456789'));
+exit;
+$query = 0;
+
+if(empty($query)) {
+	echo "查询条件为空";
+}
+if(is_numeric($query)) {
+	//9999999999 => 2286/11/21
+	$biggerThan2286 = strlen($query) > 10;
+	if($biggerThan2286){
+		echo "输入的时间戳大于2286年，重新输入";
+	}
+	date('Y-m-d H:i:s', $query);
+}
+
+return strtotime($query);
+
+function dateToStamp($date = '')
+{
+	if (empty($date)) {
+		echo "日期不能为空";
+	}
+
+	return strtotime($date);
+}
+
+function stampToData($stamp = 0)
+{
+	if (empty($stamp)) {
+		echo "时间戳不能为空";
+	}
+
+	return data('Y-m-d H:i:s');
+}
+
+?>
